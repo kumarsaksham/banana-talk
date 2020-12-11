5d4703c04257c8a8f45d881f0ee5472f8a7f3633
SAKSHAM KUMAR <64663891+kumarsaksham@users.noreply.github.com>
2020-12-11 15:15:13 +0530

    project completed

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 // References
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput     = document.querySelector("#txt-input");
-var outputDiv    = document.querySelector("#output-div")
+var btnTranslate = document.querySelector("#translate-btn");
+var translateInput     = document.querySelector("#translate-input");
+var output    = document.querySelector("#translate-output")
 
 // Event Listener
 btnTranslate.addEventListener("click", clickEventHandler);
 
+
+const serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function constructURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured " + error);
+    alert("Server is down! Please try after some time.")
+}
+
 // Callback
 function clickEventHandler() {
-    // console.log(txtInput.value);
-    // var txtInputValue = txtInput.value;
-    outputDiv.innerText = "dgdiyiidiy " + txtInput.value;
+    var input = translateInput.value;
+
+    fetch(constructURL(input))
+        .then(response => response.json())
+        .then(json => 
+            output.innerText = (json.contents.translated))
+        .catch(errorHandler)
 }
+
+
